88b0988cb54090b28e8f183c47a59bfb6f5cc142
Giri Babu <gbabu@flatironssolutions.com>
2021-11-16 22:15:57 +0530

    Added JPA Repo

---
+++ src/main/java/com/gr/microservice/CurrencyExchangeService/repo/CurrencyExchangeRepository.java
@@ -0,0 +1,13 @@
+package com.gr.microservice.CurrencyExchangeService.repo;
+
+import com.gr.microservice.CurrencyExchangeService.rest.bean.CurrencyExchange;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange, Long> {
+
+    //@Query("from CurrencyExchange C where C.from_currency=:from and C.to_currency=:to")
+    List<CurrencyExchange> findByFromAndTo(String from, String to);
+}
--- src/main/java/com/gr/microservice/CurrencyExchangeService/rest/controller/CurrencyExchangeController.java
+++ src/main/java/com/gr/microservice/CurrencyExchangeService/rest/controller/CurrencyExchangeController.java
@@ -1,11 +1,17 @@
 package com.gr.microservice.CurrencyExchangeService.rest.controller;
 
+import com.gr.microservice.CurrencyExchangeService.repo.CurrencyExchangeRepository;
 import com.gr.microservice.CurrencyExchangeService.rest.bean.CurrencyExchange;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Objects;
 
 @RestController
 public class CurrencyExchangeController {
@@ -13,12 +19,19 @@ public class CurrencyExchangeController {
     @Autowired
     Environment environment;
 
-    @GetMapping("/currency-exchange/from/USD/to/INR")
-    public CurrencyExchange getCurrencyExchangeInfo() {
-        CurrencyExchange currencyExchange = new CurrencyExchange(10001, "USD", "INR", 65.00, "8000 Instance Id");
+    @Autowired
+    CurrencyExchangeRepository repository;
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public List<CurrencyExchange> getCurrencyExchangeInfo(@NonNull @PathVariable String from, @NonNull @PathVariable String to) {
+        /*CurrencyExchange currencyExchange = new CurrencyExchange(10001, "USD", "INR", 65.00, "8000 Instance Id");
         String serverPort = environment.getProperty("server.port");
         currencyExchange.setEnvironment(serverPort);
-        return currencyExchange;
+        return currencyExchange;*/
+        List<CurrencyExchange> result = repository.findByFromAndTo(from, to);
+        if(Objects.isNull(result) || result.isEmpty())
+            throw new RuntimeException("Currency Exchange data is not available from " + from + " , to: " + to);
+        return result;
 
     }
 }
