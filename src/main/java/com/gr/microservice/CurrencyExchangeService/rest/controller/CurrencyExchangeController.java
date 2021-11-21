235a27ae3743ea116d0df512f305d5db0b3189c6
Giri Babu <gbabu@flatironssolutions.com>
2021-11-21 22:47:10 +0530

    Added Zipkin and Sleuth for Tracing of requests of all microservices

--- src/main/java/com/gr/microservice/CurrencyExchangeService/rest/controller/CurrencyExchangeController.java
+++ src/main/java/com/gr/microservice/CurrencyExchangeService/rest/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.gr.microservice.CurrencyExchangeService.rest.controller;
 
 import com.gr.microservice.CurrencyExchangeService.repo.CurrencyExchangeRepository;
 import com.gr.microservice.CurrencyExchangeService.rest.bean.CurrencyExchange;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.lang.NonNull;
@@ -22,12 +24,15 @@ public class CurrencyExchangeController {
     @Autowired
     CurrencyExchangeRepository repository;
 
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange getCurrencyExchangeInfo(@NonNull @PathVariable String from, @NonNull @PathVariable String to) {
         /*CurrencyExchange currencyExchange = new CurrencyExchange(10001, "USD", "INR", 65.00, "8000 Instance Id");
         String serverPort = environment.getProperty("server.port");
         currencyExchange.setEnvironment(serverPort);
         return currencyExchange;*/
+        logger.info("Excahnge COntroller called with from: {}, to: {}", from, to);
         CurrencyExchange result = repository.findByFromAndTo(from, to);
         if(Objects.isNull(result))
             throw new RuntimeException("Currency Exchange data is not available from " + from + " , to: " + to);
